535c4483771c7f673b9617db7a0f00dd7cfb3d6c
Ayush shukla <ayushshukla3999@gmail.com>
2025-04-24 19:06:36 +0530

    build error resolved

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import FeedbackForm from './components/FeedbackForm'
 import FeedbackList from './components/FeedbackList'
 import Auth from './components/Auth'
